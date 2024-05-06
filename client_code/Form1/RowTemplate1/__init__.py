2b2cf2492a4322f94bec5e53a1ac5fc57c60dd76
sbj314@sympatico.ca <sbj314@sympatico.ca>
2024-05-06 15:50:22 +0000

    Saved version server May 6

--- client_code/Form1/RowTemplate1/__init__.py
+++ client_code/Form1/RowTemplate1/__init__.py
@@ -1,5 +1,6 @@
 from ._anvil_designer import RowTemplate1Template
 from anvil import *
+import anvil.server
 import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
@@ -16,6 +17,12 @@ class RowTemplate1(RowTemplate1Template):
     self.item.delete()
     self.remove_from_parent()
     print('Line 18 delete')
+    pass
+  
+  def form_refreshing_data_bindings(self, **event_args):
+    
+    """This method is called when refreshing_data_bindings is called"""
+  
     pass
 
 # class RowTemplate1(RowTemplate1Template):
@@ -39,7 +46,7 @@ class RowTemplate1(RowTemplate1Template):
 
 #     # Any code you write here will run before the form opens.
 
-#     def form_refreshing_data_bindings(self, **event_args):
-#       """This method is called when refreshing_data_bindings is called"""
+    def form_refreshing_data_bindings(self, **event_args):
+      """This method is called when refreshing_data_bindings is called"""
 #     pass
 
